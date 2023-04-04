f9f832b85083e14ae615b5c7407e60a3a28ed1b6
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-04-04 09:11:16 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Evelyn Chen on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
